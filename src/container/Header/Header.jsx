90280e44937f044286705531a3a125b96174e600
junaif <cpjunaif98@gmail.com>
2022-09-21 10:45:46 +0530

    commit done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import {SubHeading} from '../../components'
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className="app__wrapper_indo">
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+    <button type='button' className='custom__button'>Explore More</button>
+    </div>
+    <div className="app__wrapper-img">
+     <img src={images.welcome}   alt="header img" />
+    </div>
   </div>
 );
 
